7f90afc0241f4449d3871f1225665f63c8545ef7
Andres Rossemberg <andres.rossemberg.m.d.l@gmail.com>
2026-09-12 01:58:44 -0500

    refactor: añadir const y key para resolver warnings

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget{
+
+  const MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Center(
-        child: Text('Bolsa de Empleo IDEFUP')
+        child: Text('Bolsa de Empleo IDEFUP!')
       ),
     );
   }
