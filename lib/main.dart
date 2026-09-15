d75fd1189ff4b65e3c7efa33ef9e64f2281d7067
Andres Rossemberg <andres.rossemberg.m.d.l@gmail.com>
2026-09-15 00:19:08 -0500

    feat: aplicar tema Material 3 y AppBar a pantalla de bienvenida

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,22 @@ void main(){
 }
 
 class MyApp extends StatelessWidget{
-
   const MyApp({super.key});
   
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: WelcomeScreen()
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.lightBlue,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.lightBlue.shade700,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+        )
+      ),
+      home: const WelcomeScreen()
     );
   }
 }
--- lib/presentation/screens/welcome/welcome_screen.dart
+++ lib/presentation/screens/welcome/welcome_screen.dart
@@ -5,8 +5,11 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Bienvenido a la bolsa de empleo IDEFUP!'))
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bienvenido'),
+      ),
+      body: const Center(child: Text('Bienvenido a la bolsa de empleo IDEFUP!'))
     );
   }
 }
